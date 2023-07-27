bfeaceeab6bbf713b8ffba00be8d8126a283d0c6
nithin9977 <114212568+nithin9977@users.noreply.github.com>
2023-07-27 22:54:07 +0530

    Create VerificationTokendao.java

---
+++ LibraryManagementSystem/src/main/project/dao/VerificationTokendao.java
@@ -0,0 +1,15 @@
+package edu.sjsu.cmpe275.project.dao;
+
+import edu.sjsu.cmpe275.project.model.User;
+import edu.sjsu.cmpe275.project.model.VerificationToken;
+
+public interface VerificationTokenDao {
+    VerificationToken findByToken(String token);
+
+    VerificationToken findByUser(User user);
+
+    VerificationToken save(VerificationToken verificationToken);
+
+    void delete(VerificationToken verificationToken);
+
+}
